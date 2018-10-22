c2ea397b3dd48b237f6cbcd3bdde4387592e5d09
Sam Benemerito <benemeritosam@gmail.com>
2018-10-22 15:27:14 +0900

    initial Fund model, migrations

--- fund/models.py
+++ fund/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = ((1, 'Personal'), (2, 'Education'), (3, 'Community'),
+                    (4, 'Health & Medicine'), (5, 'Special Events'))
+STATUS_CHOICES = ((1, 'Ongoing'), (2, 'Completed'), (3, 'Ended'))
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.PositiveSmallIntegerField(choices=CATEGORY_CHOICES)
+    description = models.TextField()
+    date_created = models.DateField(auto_now=True)
+    date_goal = models.DateField()
+    date_finished = models.DateField(null=True, blank=True)
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES)
+    amount_funded = models.FloatField(default=0)
+    amount_goal = models.FloatField()
+    creator_address = models.CharField(max_length=100)
