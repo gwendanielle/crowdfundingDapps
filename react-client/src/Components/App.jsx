c71a2243950acd9893a127706cd2aa5adb30f462
Sam Benemerito <benemeritosam@gmail.com>
2018-10-23 22:29:22 +0900

    add web3 instance, and contract

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -7,6 +7,125 @@ import FundListPage from './FundListPage'
 import NavBar from './HeaderComponent/NavBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    if (typeof web3 != 'undefined') {
+      console.log("Using web3 detected from external source like MetaMask");
+      this.web3 = new Web3(web3.currentProvider);
+    } else {
+      this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+    }
+
+    const MyContract = web3.eth.contract([
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_fundGoal",
+        "type": "uint256"
+      }
+    ],
+    "name": "addProject",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_project",
+        "type": "address"
+      }
+    ],
+    "name": "fund",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "kill",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "projectExists",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "projectInfo",
+    "outputs": [
+      {
+        "name": "fundGoal",
+        "type": "uint256"
+      },
+      {
+        "name": "currentFund",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }]);
+    this.state = {
+      ContractInstance: MyContract.at("0xda1442d2774d43d2316b3329f8238724891b009b")
+    };
+  }
   render() {
     return (
       <Router>
