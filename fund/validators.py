54ca7c2cdfe9aa9368c586c4691d7388170ea572
Sam Benemerito <benemeritosam@gmail.com>
2018-10-25 16:53:04 +0900

    add some docstrings

--- fund/validators.py
+++ fund/validators.py
@@ -6,17 +6,20 @@ from fund.models import Project
 
 
 def validate_date_goal(value):
+    """Validate date_goal to be later than current day"""
     now = datetime.now().date()
     if value and (value - now).days < 1:
         raise ValidationError('Goal date should be at least 1 day from now')
 
 
 def validate_amount_goal(value):
+    """Validate amount_goal to be greater than 0"""
     if not value or value < 0:
         raise ValidationError('Goal amount should be greater than 0')
 
 
 def validate_creator_address(value):
+    """Validate creator_address to not have another project"""
     projects_by_creator_address = Project.objects.filter(creator_address=value, status=1)
     if projects_by_creator_address.exists():
         raise ValidationError('Each address can only have one fundraising project at a time')
